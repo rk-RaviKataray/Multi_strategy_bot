4ad8c9987458bd0905ad20272c3a0e070e863d71
rk-RaviKataray <72041035+rk-RaviKataray@users.noreply.github.com>
2023-07-26 01:15:29 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,7 +1,1 @@
-from UltraDict import UltraDict
-other = UltraDict(recurse = True, name='ravi_',create=False,auto_unlink=False)
-other['nested']['counter'] += 1
 
-print(other)
-
-#secret!!!!!!!!!!!!!
